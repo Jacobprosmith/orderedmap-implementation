b3b8afdb3f1c732a24165523db5b733c3cb0a73f
Jacob Proenza-Smith <116226571+Jacobprosmith@users.noreply.github.com>
2023-12-01 13:18:29 -0500

    Delete proj5_provided directory

--- proj5_provided/hashtable.h
+++
@@ -1,7 +0,0 @@
-// used when improper size is given (for example
-// size is 0 or negatiave)
-static const unsigned int default_capacity = 11;
-static const unsigned int max_prime = 1301081;
-
-unsigned long prime_below(long);
-void setPrimes(std::vector<long> &);
